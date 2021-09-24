08c9bf10292c9d4dd489051d7dfc54a2d6ffad43
Frogodrig <72153510+Frogodrig@users.noreply.github.com>
2021-09-24 23:03:38 +0530

    Greedy Technique
    https://www.spoj.com/problems/EXPEDI/

---
+++ expedi.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+
+    while(t--) {
+
+        int n;cin>>n;
+
+        vector<pair<int,int>> a(n);
+
+        for(int i=0;i<n;i++) {
+
+            cin>> a[i].first >> a[i].second;
+        }
+
+        int l,p;
+        cin>>l>>p;
+
+        for(int i=0;i<n;i++) {
+            a[i].first = l - a[i].first;
+        }
+
+        sort(a.begin(),a.end());
+
+        int ans = 0;
+        int curr = p;
+
+        priority_queue<int, vector<int> >pq;
+        bool flag = 0;
+        for(int i=0;i<n;i++) {
+            if(curr >= l) {
+                break;
+            }
+
+            while(curr < a[i].first) {
+                if(pq.empty()) {
+                     flag = 1;
+                     break;
+                }
+                ans++;
+                curr += pq.top();
+                pq.pop();
+            }
+
+            if(flag) {
+                break;
+            }
+
+            pq.push(a[i].second);
+        }
+
+        if(flag) {
+            cout<<"-1"<<"\n";
+            continue;
+        }
+
+        while(!pq.empty() && curr < l) {
+            curr += pq.top();
+            pq.pop();
+            ans++;
+        }
+
+        if(curr < l) {
+            cout<<"-1"<<"\n";
+            continue;
+        }
+
+        cout<<ans<<"\n";
+    }
+
+    return 0;
+}
